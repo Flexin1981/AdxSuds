048f643921fd291b262cac80fbc68531805419cf
John Dowling <johndowling01@live.co.uk>
2016-06-01 08:51:09 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='AdxSuds',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/Flexin1981/AdxSuds',
+    license='',
+    author='John Dowling',
+    author_email='johndowling01@live.co.uk',
+    description='Brocade Adx Suds Module for the XML Api'
+)
